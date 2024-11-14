bfbb9ca0278555711cb2bcc30d6f4a1b3382344a
abdulraheem 5353 <abdulraheemmalik6565@gmail.com>
2024-11-14 05:08:56 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Web Developer", "ReactJS Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
